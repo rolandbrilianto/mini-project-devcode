d07dc8c5fe0ec569c376fc263f49283d35439501
Roland Brilianto <rolandbrilianto@gmail.com>
2024-08-16 16:56:35 +0700

    feat(component):create header component

---
+++ src/components/header-background.vue
@@ -0,0 +1,8 @@
+<template>
+  <header
+    data-cy="header-background"
+    class="sticky h-[64px] sm:h-[105px] bg-main-blue flex items-center"
+  >
+    <slot></slot>
+  </header>
+</template>
---
+++ src/components/header-title.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1
+    data-cy="heaeder-title"
+    class="uppercase font-bold text-lg sm:text-2xl text-white ml-5 sm:ml-[220px]"
+  >
+    to do list app
+  </h1>
+</template>
