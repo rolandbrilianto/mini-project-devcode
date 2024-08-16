ae869fbc61e8b34c8e81b34680141bcd83beb0e2
Roland Brilianto <rolandbrilianto@gmail.com>
2024-08-16 16:57:42 +0700

    feat(view):create sample view for test

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,3 +1,15 @@
 <template>
+  <HeaderBackground>
+    <HeaderTitle />
+  </HeaderBackground>
   <h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
+  <activityItem />
+  <activityAddButton />
 </template>
+
+<script setup>
+import HeaderBackground from "@/components/header-background.vue";
+import HeaderTitle from "@/components/header-title.vue";
+import activityItem from "@/components/activity-item.vue";
+import activityAddButton from "@/components/activity-add-button.vue";
+</script>
