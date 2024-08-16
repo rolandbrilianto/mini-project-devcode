8df8af78bc81764364209b75d359a710b5e39a42
Roland Brilianto <rolandbrilianto@gmail.com>
2024-08-16 16:57:24 +0700

    feat(tailwind):add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-blue": "#16ABF8",
+      },
+      boxShadow: {
+        "main-box": "0px 6px 10px 0px #0000001A",
+      },
+    },
   },
   plugins: [],
 };
